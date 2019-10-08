b1662dcdb4e5dcdaa863f11a82657670ae68de13
a.khan <a.khan4@students.clark.edu>
2019-10-08 15:18:13 -0700

    lab 3 done

---
+++ TuesDemo-10-08/DesicionConstructs/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesicionConstructs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region if/else examples
+
+            
+            if (true)
+            {
+                Console.WriteLine(true);
+            }
+            if (!false)
+            {
+                Console.WriteLine(false);
+            }
+
+            if (false)
+            {
+                Console.WriteLine(false);
+            }  else
+            {
+                Console.WriteLine("if/else {0}", false);
+            }
+            #endregion
+
+
+            #region switch statement example
+            for (int i = 0; i < 6; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        Console.WriteLine(i);
+                        break;
+                    case 1:
+                        Console.WriteLine(i);
+                        break;
+                    case 2:
+                    case 3:
+                        Console.WriteLine("2 or 3: {0}",i);
+                        break;
+
+                    default:
+                        Console.WriteLine("default: {0}", i);
+                        break;
+                }
+            }
+            #endregion
+
+        }
+    }
+}
---
+++ TuesDemo-10-08/Itterations/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itterations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region siple for loop
+
+            Console.WriteLine("Simple for loop example");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+
+            }
+            Console.WriteLine();
+
+            #endregion
+
+            #region nested for loops
+
+            Console.WriteLine("Nested for loop example");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine("i= {0}, j= {1}", i, j);
+                }
+
+            }
+            #endregion
+
+            #region foreach loop
+            Console.WriteLine("foreach element in array example");
+            int[] myInts = { 1, 2, 3, 4, 5 };
+            for (int i = 0; i < myInts.Length; i++)
+            {
+                myInts[i] = i + 10;
+            }
+
+            foreach (int element in myInts)
+            {
+                Console.WriteLine(element);
+            }
+
+            #endregion
+
+            #region while loop
+
+            int count = 0;
+            while (count < myInts.Length)
+            {
+                Console.WriteLine("{0}", myInts[count]);
+                count++;
+            }
+
+            #endregion
+
+            #region do/while loop
+
+            Console.WriteLine();
+            do
+            {
+                Console.WriteLine("{0}", myInts[0]);
+                count++;
+            }
+            while (count < myInts.Length)
+
+            #endregion
+        }
+    }
+}
